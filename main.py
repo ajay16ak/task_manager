9ef6080c73e5ea12b8e5943cdea9b06c12c889dc
Ajay kumar <kuruvaajay16@gmail.com>
2026-05-11 09:55:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from routers import tasks, auth
+import tasks
+import auth
 import os
 
 app = FastAPI(
