c7630baeac5409111da54245709f070cdf08b9ab
Ajay kumar <kuruvaajay16@gmail.com>
2026-05-11 11:53:46 +0530

    Update Register.tsx

--- Register.tsx
+++ Register.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { authApi } from '../api';
+import { authApi } from './api';
 import { KeyRound, Mail, User } from 'lucide-react';
 
 export default function Register() {
